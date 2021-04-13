cbc147d827f0f8354e5db7173a3c14d2e6abdbe4
droal <droal.tech@gmail.com>
2021-04-13 16:50:47 -0500

    Se implementó BrowserRouter, Route, Switch y Link para controlar la navegación

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import './global.css'
-import Badges from './pages/Badges'
+import App from './components/App'
 
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__); 
 ReactDOM.render(
-  <Badges/>,
+  <App/>,
   container
 );
