f057c43f49fb71ad55140d025516e01f6ebd8e4e
droal <droal.tech@gmail.com>
2021-04-14 15:04:22 -0500

    Request Get usando fetch, manejo de respuesta de error y datos vacíos

--- src/components/BadgestList.js
+++ src/components/BadgestList.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 import './styles/BadgestList.css';
 
 class BadgesListItem extends React.Component {
+  
   render() {
     return (
       <div className="BadgesListItem">
@@ -28,6 +30,17 @@ class BadgesListItem extends React.Component {
 
 class BadgesList extends React.Component {
   render() {
+     if(this.props.badges.length === 0){
+      return(
+        <div>
+          <h3>No badges were found</h3>
+          <Link className= "btn btn-primary" to="/badges/new">
+            Create new badge
+          </Link>
+        </div>
+      )
+    }
+
     return (
       <div className="BadgesList">
         <ul className="list-unstyled">
